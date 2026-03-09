2c302c9b1534de02ba35a0681c7204f2a95ceb54
Md Faizan Ahmad <mdfaizan7563@gmail.com>
2026-03-10 00:04:39 +0530

    feat: update datebutton.tsx

--- src/booking/mobile/steps/DateButton.tsx
+++ src/booking/mobile/steps/DateButton.tsx
@@ -14,7 +14,7 @@ export default function DateButton({
   return (
     <button
       onClick={() => onSelect(date)}
-      className={`py-3 text-[10px] font-bold border transition-all ${
+      className={`py-3 cursor-pointer text-[10px] font-bold border transition-all ${
         selected
           ? "bg-sky-700 text-white border-sky-700"
           : "bg-slate-50 border-slate-100 text-slate-600"
